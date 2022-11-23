5d9009eaeba847f5d13ae85b668da67bea0c76dc
Denis.Zakusilo <dvzakusilo@gmail.com>
2022-11-23 11:31:19 +0300

    Add meta

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name') }}</title>
-        <meta name="keywords" content="создание сайтов, bitrix, битрикс, разработка сайтов, сделать сайт, laravel, vue">
-        <meta name="description" content="Разработка и создание сайтов любой сложности по низким ценам">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link rel="icon" type="image/x-icon" href="/images/index/favicon.ico">
-    </head>
-    <body>
-        <div id="app"></div>
-        <script>
-            window.config = @json([
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name') }}</title>
+    <meta name="keywords" content="создание сайтов, bitrix, битрикс, разработка сайтов, сделать сайт, laravel, vue">
+    <meta name="description" content="Разработка и создание сайтов любой сложности по низким ценам">
+    <meta property="og:image"
+          content="https://web-vector.ru/images/index/top_image.png">
+    <meta property="og:image:width" content="1280">
+    <meta property="og:image:height" content="670">
+    <meta name="twitter:image"
+          content="https://web-vector.ru/images/index/top_image.png">
+    <meta name="vk:image"
+          content="https://web-vector.ru/images/index/top_image.png">
+    <meta property="og:image" content="https://web-vector.ru/images/index/top_image.png">
+    <meta property="og:image:width" content="400">
+    <meta property="og:image:height" content="400">
+    <meta property="og:title"
+          content="Разработка и создание сайтов любой сложности по низким ценам">
+    <meta property="og:description"
+          content="Разработка и создание сайтов любой сложности по низким ценам">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="/images/index/favicon.ico">
+</head>
+<body>
+<div id="app"></div>
+<script>
+    window.config = @json([
                 'appName' => config('app.name'),
                 'deviceName' => 'spa'
             ])
-        </script>
-        <script src="{{ asset("js/app.js") }}" charset="utf-8"></script>
-    </body>
+</script>
+<script src="{{ asset("js/app.js") }}" charset="utf-8"></script>
+</body>
 </html>
